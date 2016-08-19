3edbb51199e91404d45cd472bc6796cab589dca6
WilliamCasasola <wacvillalobos@hotmail.es>
2016-08-19 09:14:24 -0600

    Agregar archivos de proyecto.

---
+++ SGA/Models/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SGA.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public String clave { set; get; }
+        [Required]
+        [StringLength(70)]
+        [Display(Name = "Nombre")]
+        public String nombre  { set; get; }
+        [Required]
+        [StringLength(70)]
+        [Display(Name = "País")]
+        public String pais { set; get; }
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "Teléfono")]
+        public String telefono { set; get; }
+        [Required]
+        [StringLength(70)]
+        [Display(Name = "Correo")]
+        public String correo { set; get; }
+        [Required]
+        [StringLength(70)]
+        [Display(Name = "Dirección")]
+        public String direccion { set; get; }
+    }
+}
---
+++ SGA/Models/Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SGA.Models
+{
+    public class Persona : Cliente
+    {
+      
+        [Required]
+        [StringLength(70)]
+        [Display(Name = "Apellidos")]
+        public String apellidos { set; get; }
+        [StringLength(4, ErrorMessage = "Máximo x dígitos.")]
+        [Column("contrasena")]
+        public String contrasena { set; get; }
+        [Required]
+        [Display(Name = "Sexo")]
+        public Boolean sexo { set; get; }
+        [Required]
+        [Display(Name = "Profesión")]
+        public String profesion { set; get; }
+        [Required]
+        [Display(Name = "Institución")]
+        public String institucion { set; get; }
+        public String rutaFoto { set; get; }
+        [Required]
+        [Display(Name = "Correo Alternativo")]
+        public String correoAlternativo { set; get; }
+    }
+}
